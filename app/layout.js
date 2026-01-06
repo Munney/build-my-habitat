598cddf1a3b6fd2a640c6a1bb2e6b0ff17fa2619
Munney <munnellyryan@gmail.com>
2026-01-06 18:19:01 -0500

    Add GTM script directly in head tag for immediate detection

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Image from "next/image";
+import Script from "next/script";
 import Navbar from "./components/Navbar"; // 👈 Import the new file
 import Analytics from "./components/Analytics";
 import GTM from "./components/GTM";
@@ -13,11 +14,27 @@ export const metadata = {
     default: 'HabitatBuilder - Smart Reptile & Aquarium Setup Configurator',
   },
   description: 'Stop guessing at the pet store. Build safe, complete, and species-appropriate habitats for Leopard Geckos, Betta Fish, and more with our smart configurator.',
+  other: {
+    'google-tag-manager': 'GTM-P92HFFRX',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager - Direct script injection */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P92HFFRX');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${inter.className} bg-[#020617] text-slate-200 min-h-screen flex flex-col`}>
         {/* Google Tag Manager */}
         <GTM />
