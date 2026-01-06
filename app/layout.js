25fd2ad306b8970d86b8b2e11cf7d55f1450c447
Munney <munnellyryan@gmail.com>
2026-01-06 18:11:56 -0500

    Fix GTM script placement - beforeInteractive automatically injects to head

--- app/layout.js
+++ app/layout.js
@@ -18,21 +18,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag Manager - Loads in head with beforeInteractive */}
-        <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      {/* Google Tag Manager - Loads in head with beforeInteractive */}
+      <Script
+        id="google-tag-manager"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-P92HFFRX');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </head>
+        }}
+      />
+      {/* End Google Tag Manager */}
       <body className={`${inter.className} bg-[#020617] text-slate-200 min-h-screen flex flex-col`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
