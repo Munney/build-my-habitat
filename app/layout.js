338caf72443da4ad0f5b80f917945f54b2bf1c3f
Munney <munnellyryan@gmail.com>
2026-01-12 01:43:22 -0500

    Prioritize 32x32 favicon for better visibility in browser tabs

--- app/layout.js
+++ app/layout.js
@@ -16,9 +16,9 @@ export const metadata = {
   authors: [{ name: 'HabitatBuilder' }],
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
     ],
     apple: '/apple-touch-icon.png',
   },
@@ -102,10 +102,10 @@ export default function RootLayout({ children }) {
           }}
         />
         
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        {/* Favicon - 32x32 first for better visibility in browser tabs */}
         <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         
         {/* Web App Manifest */}
